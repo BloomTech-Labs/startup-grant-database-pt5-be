358844dcc0fc871ea851ebd9d18c5e0c1c29206b
Scott Schindler <scottschindler29@gmail.com>
2019-10-24 23:39:19 -0400

    set up an endpoint to get all users

--- database/DbConfig.js
+++ database/DbConfig.js
@@ -1,7 +1,8 @@
 const knex = require("knex");
+require("dotenv").config();
 
 const config = require("../knexfile.js");
 
 const dbEnv = process.env.DB_ENV || "development";
-
+console.log(dbEnv);
 module.exports = knex(config[dbEnv]);
--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 //Server
 const express = require("express");
+
 const server = express();
+const cors = require("cors");
 
+server.use(cors());
 server.use(express.json());
-
+const UserRouter = require("../database/users/user-router.js");
 server.get("/", (req, res) => {
   res.status(200).send("<h3>Hello World!!!</h3>");
 });
+
+server.use("/api/users", UserRouter);
 
 module.exports = server;
