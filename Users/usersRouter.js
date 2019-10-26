459d4fbf7092e47cc104bf2186b4e6b87c576301
fritzgt <tuanis@live.com>
2019-10-26 16:39:05 +0300

    handle errors and tested

--- Users/usersRouter.js
+++ Users/usersRouter.js
@@ -10,7 +10,8 @@ const verify = require('../auth/restricted-middleware');
 //==========================================================================
 //Login user or create a new user if a current user is not found
 router.post('/', verify, async (req, res) => {
-  console.log('Token from HTTP request: ', req.body.decodedToken);
+  // console.log('Passed middleware from HTTP request: ', req.body.decodedToken);
+  //Deconstructing decoded token
   const userInfo = req.body.decodedToken;
   const name = userInfo.name;
   const email = userInfo.email;
@@ -20,11 +21,11 @@ router.post('/', verify, async (req, res) => {
   //
   // [x] Pass the rest of user info life first and last name
   // [x] FIX DATE TO USE JS FUNCTION
-  // [ ] Handle errors correctly
+  // [x] Handle errors correctly
   // [x] Hide private keys from firebase
 
   //Date format for timestamp
-  var today = new Date();
+  var timeStamp = new Date();
   var dd = today.getDate();
   var mm = today.getMonth() + 1;
   var yyyy = today.getFullYear();
@@ -35,7 +36,7 @@ router.post('/', verify, async (req, res) => {
     first_name: name,
     email: email,
     uid: uid,
-    created_at: today
+    created_at: timeStamp
   };
   // console.log(creds);
   try {
@@ -43,16 +44,17 @@ router.post('/', verify, async (req, res) => {
     const currentUser = await DB.findByEmail(email);
     // console.log('CurrentUser', currentUser);
 
+    //LOGIC TO SIGNUP OR LOGIN USER
     //if request is not an empty object then an account was found and the id is return
     if (`${currentUser}`) {
-      res.status(200).json({ Found: currentUser[0] });
+      res.status(200).json({ found: currentUser[0] });
       //if request is an empty object then an account is created and the id is return
     } else {
       const newUser = await DB.add(creds);
-      res.status(201).json({ Created: newUser });
+      res.status(201).json({ created: newUser });
     }
   } catch (err) {
-    console.log('Error during http request', err);
+    console.log('Error during LOGIN/SIGNUP http request', err);
     res.status(400).json({ message: err.message });
   }
 });
