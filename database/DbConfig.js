a0cda5b516b511755ec2354a9f6ca19bb1097487
Ernesto Pena <ernestopenad@gmail.com>
2019-10-28 21:56:03 -0500

    test

--- database/DbConfig.js
+++ database/DbConfig.js
@@ -3,6 +3,6 @@ require("dotenv").config();
 
 const config = require("../knexfile.js");
 
-const dbEnv = process.env.DB_ENV || "development";
+const dbEnv = "staging"// process.env.DB_ENV || "development";
 console.log(dbEnv);
 module.exports = knex(config[dbEnv]);
