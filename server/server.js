2b6b390799af384e3cca02ecf62df8090979712d
Ernesto Pena <ernestopenad@gmail.com>
2019-11-11 22:20:02 -0600

    Routes and database models were implemented for State, Counties, Elegibility, and Category tables. This is to provide endpoints to the front end

--- server/server.js
+++ server/server.js
@@ -11,11 +11,19 @@ server.use(cors());
 // const userRoutes = require('../Users/usersRouter');
 const userRouter = require('../routes/users/user-router.js');
 const grantRouter = require('../routes/grants/grants_router.js');
+const states = require('./../routes/states_cities/states_route.js')
+const counties = require('./../routes/counties/counties_router.js');
+const elegibility = require('./../routes/elegibility/elegibility_router.js');
+const categories = require('./../routes/categories/categories_router.js');
 
 //IMPLEMENTING ROUTES
 // server.use('/api/login', userRoutes);
 server.use('/api/users', userRouter);
 server.use('/api/grants', grantRouter);
+server.use('/api/states', states);
+server.use('/api/counties', counties);
+server.use('/api/elegibility', elegibility);
+server.use('/api/categories', categories);
 
 server.get('/', (req, res) => {
   res.status(200).send('<h3>Hello World!!!</h3>');
