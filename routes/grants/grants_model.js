60d7eb8af29ccf03b68c1bd4485cb7b0a5fec094
Ernesto Pena <ernestopenad@gmail.com>
2019-11-19 19:47:51 -0600

    Working on the master search result database model

--- routes/grants/grants_model.js
+++ routes/grants/grants_model.js
@@ -12,46 +12,47 @@ module.exports = {
 
 //Function to obtain all grants by different parameters
 function masterSearch(state=2, counties='', amaount='', elegibility='', categories='') {
-    // return db('grants as G').innerJoin('users AS U', 'G.user_id','U.id')
-    //                         .innerJoin('regions AS R', 'G.id' , 'R.grant_id')
-    //                         .innerJoin('states AS S', 'R.state_id', 'S.id' )
-    //                         .innerJoin('counties AS C', 'R.county_id', 'C.id')
-    //                         .innerJoin('elegibility_grants AS EG', 'G.id', 'EG.grants_id')
-    //                         .innerJoin('elegibility AS E', 'E.id','EG.elegibility_id')
-    //                         .innerJoin('category_grants AS CG', 'G.id', 'CG.grants_id')
-    //                         .innerJoin('category_keys AS CK','CK.id','CG.category_id')
-    //                         .select('G.grant_title','G.grant_number','G.grant_status','G.grant_description','G.grant_amount','G.due_date',
-    //                                 'U.first_name','U.last_name','U.email','U.telephone','U.department','U.organization_name','U.address_one','U.address_two','U.zip_code',
-    //                                 'R.country_wide','R.state_id','R.county_id',
-    //                                 'S.state_name',
-    //                                 'C.county_name',
-    //                                 'EG.grants_id','EG.elegibility_id',
-    //                                 'E.elegibility_name',
-    //                                 'CG.category_id',
-    //                                 'CK.category_name'
-    //                                 )
-    //                         // .where('R.state_id', '=',  state)
-
-        return db('grants as G')
-                            .innerJoin('users AS U', 'G.user_id','U.id')
-                            .innerJoin('regions AS R', 'G.id' , 'R.grant_id')
-                            .leftJoin('states AS S', 'R.state_id', 'S.id' )
-                            .leftJoin('counties AS C', 'R.county_id', 'C.id')
-                            .innerJoin('elegibility_grants AS EG', 'G.id', 'EG.grants_id')
-                            .innerJoin('elegibility AS E', 'E.id','EG.elegibility_id')
-                            .innerJoin('category_grants AS CG', 'G.id', 'CG.grants_id')
-                            .innerJoin('category_keys AS CK','CK.id','CG.category_id')
-                            .select('G.id','G.grant_title','G.grant_number','G.grant_status','G.grant_description','G.grant_amount','G.due_date',
-                                    'U.first_name','U.last_name','U.email','U.telephone','U.department','U.organization_name','U.address_one','U.address_two','U.zip_code',
-                                    'R.country_wide','R.state_id','R.county_id',
-                                    'S.state_name',
-                                    'C.county_name',
-                                    'EG.grants_id','EG.elegibility_id',
-                                    'E.elegibility_name',
-                                    'CG.category_id',
-                                    'CK.category_name'
-                                    )
-                            // .where('R.state_id', '=',  state)
-
-                            
+        return db('grants as g').innerJoin('users AS u', 'g.user_id','u.id')
+                                .innerJoin('regions AS r', 'g.id' , 'r.grant_id')
+                                .leftJoin('states AS s', 'r.state_id','=','s.id')
+                                .leftJoin('counties AS c', 'r.county_id', 'c.id')
+                                .innerJoin('elegibility_grants AS eg', 'g.id', 'eg.grants_id')
+                                .innerJoin('elegibility AS e', 'e.id','eg.elegibility_id')
+                                .innerJoin('category_grants AS cg', 'g.id', 'cg.grants_id')
+                                .innerJoin('category_keys AS ck','ck.id','cg.category_id')
+                                .leftJoin('grants_modification_history AS gm', 'gm.grant_id', 'g.id')
+                                .select('g.id',
+                                        'g.grant_title',
+                                        'g.grant_number',
+                                        'g.grant_status',
+                                        'g.grant_description',
+                                        'g.grant_amount',
+                                        'g.due_date',
+                                        'u.first_name',
+                                        'u.last_name',
+                                        'u.email',
+                                        'u.telephone',
+                                        'u.department',
+                                        'u.organization_name',
+                                        'u.address_one',
+                                        'u.address_two',
+                                        'u.zip_code',
+                                        db.raw('array_agg(DISTINCT s.state_name) as states'),
+                                        db.raw('array_agg(DISTINCT e.elegibility_name) as elegibilities'),
+                                        db.raw('array_agg(DISTINCT ck.category_name) as categories'),
+                                        db.raw('array_agg(DISTINCT gm.modification_description) as history')  //json_agg(to_chart(gm.updated_at, "MM:DD:YYYY"))
+                                     )
+                                .groupBy('g.id',
+                                         'u.first_name',
+                                         'u.last_name',
+                                         'u.email',
+                                         'u.telephone',
+                                         'u.department',
+                                         'u.organization_name',
+                                         'u.address_one',
+                                         'u.address_two',
+                                         'u.zip_code'
+                                )
+                                .orderBy('g.id')
+                            // .where('R.state_id', '=',  state)                           
 };
