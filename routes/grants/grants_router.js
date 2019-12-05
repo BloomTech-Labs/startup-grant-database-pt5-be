5c650fbb97cff6df8868df6d5deb4d1605392494
Claire Sinozich <claire.sinozich@gmail.com>
2019-12-05 08:36:49 -0500

    small endpoint tweak

--- routes/grants/grants_router.js
+++ routes/grants/grants_router.js
@@ -1,29 +1,38 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('./grants_model.js');
+const db = require("./grants_model.js");
 
-//GET endpoint to ontain all grants
-// router.get('/', async (req,res) => {
-//     try {
-//         const grantResult = await db.find();
-//         res.status(200).json(grantResult);
-//     }
-//     catch (err) {
-//         res.status(500).json({message: err.message});
-//     }
-// });
+// GET endpoint to ontain all grants
+router.get("/", async (req, res) => {
+  try {
+    const grantResult = await db.find();
+    res.status(200).json(grantResult);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 //GET endpoint to obtain all grants matching state. counties, amount elegibility, and categories
-router.get('/search', async (req,res) => {
-    try {
-        const { states, counties, amount, elegibility, category } = req.params;
+router.get("/search", async (req, res) => {
+  try {
+    const { states, counties, amount, elegibility, category } = req.params;
 
-        const grantSearch = await db.masterSearch(states, counties, amount, elegibility, category);
-        res.status(200).json(grantSearch);
-    }
-    catch (err) {
-        res.status(500).json({Message: 'There was an error with your request', Error: err.message});
-    }
-})
+    const grantSearch = await db.masterSearch(
+      states,
+      counties,
+      amount,
+      elegibility,
+      category
+    );
+    res.status(200).json(grantSearch);
+  } catch (err) {
+    res
+      .status(500)
+      .json({
+        Message: "There was an error with your request",
+        Error: err.message
+      });
+  }
+});
 
 module.exports = router;
