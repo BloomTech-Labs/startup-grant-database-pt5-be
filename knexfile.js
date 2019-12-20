e8b46d6e16f2ba16023ad9ce0c9bd9430f1f510b
Jonathan Traverso <jtraverso7@gmail.com>
2019-12-19 21:56:05 -0500

    Push for debugging.

--- knexfile.js
+++ knexfile.js
@@ -1,25 +1,27 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
-    client: 'pg',
+    client: "pg",
     connection: {
-      host: 'localhost',
-      user: 'postgres',
-      database: 'grants',
-      user: 'postgres',
-      password: 'ert39883988'
+      host: "localhost",
+      user: "postgres",
+      database: "grants",
+      user: "postgres",
+      password: "ert39883988"
     },
     migrations: {
-      directory: './database/migrations'
+      directory: "./database/migrations"
     },
     seeds: {
-      directory: './database/seeds'
+      directory: "./database/seeds"
     }
   },
 
   staging: {
-    client: process.env.DB_STAGING_CLIENT,
+    // client: process.env.DB_STAGING_CLIENT,
+
+    client: "pg",
     connection: {
       host: process.env.DB_STAGING_HOST,
       database: process.env.DB_STAGING,
@@ -30,18 +32,18 @@ module.exports = {
       }
     },
     migrations: {
-      directory: './database/migrations'
+      directory: "./database/migrations"
     },
     seeds: {
-      directory: './database/seeds'
+      directory: "./database/seeds"
     }
   },
 
   production: {
-    client: process.env.DB_PRODUCTION_CLIENT,
+    client: "pg",
     connection: {
       host: process.env.DB_PRODUCTION_HOST,
-      database: 'dad7aiuhqookm7',
+      database: "dad7aiuhqookm7",
       user: process.env.DB_PRODUCTION_USER,
       password: process.env.DB_PRODUCTION_PASSWORD,
       ssl: {
@@ -49,30 +51,30 @@ module.exports = {
       }
     },
     migrations: {
-      directory: './database/migrations'
+      directory: "./database/migrations"
     },
     seeds: {
-      directory: './database/seeds'
+      directory: "./database/seeds"
     }
   },
   testing: {
-    client: 'pg',
+    client: "pg",
     connection: {
-      host: 'ec2-174-129-252-255.compute-1.amazonaws.com',
-      database: 'devhmhuq6b15ch',
-      user: 'kmzzotiiduedrv',
+      host: "ec2-174-129-252-255.compute-1.amazonaws.com",
+      database: "devhmhuq6b15ch",
+      user: "kmzzotiiduedrv",
       password:
-        '4d786fe178ad7a77e8f2f93c417c470dc185932ea8fc9075650494052aff53c4',
+        "4d786fe178ad7a77e8f2f93c417c470dc185932ea8fc9075650494052aff53c4",
       ssl: {
         rejectUnauthorized: false
       }
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './database/migrations'
+      directory: "./database/migrations"
     },
     seeds: {
-      directory: './database/seeds'
+      directory: "./database/seeds"
     }
   }
 };
