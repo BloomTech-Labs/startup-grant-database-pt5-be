8ff58abd22f5cdd159812e6c7bc4087ac279816a
Jonathan Traverso <jtraverso7@gmail.com>
2020-01-08 22:47:25 -0500

    Ability to accept or deny applications, implemented.

--- routes/grants/grant_apps_model.js
+++ routes/grants/grant_apps_model.js
@@ -3,7 +3,9 @@ const db = require("../../database/DbConfig.js");
 module.exports = {
   find,
   findById,
-  add
+  add,
+  createStatus,
+  updateStatus
 };
 
 // returns all grants
@@ -11,12 +13,33 @@ function find() {
   return db("grant_applications");
 }
 
-// returns a user's grants
-function findById(recipientId) {
-  return db("grant_applications").where({ id: recipientId });
+// returns a grant's applications
+function findById(grantId) {
+  return db("grant_applications").where({ grant_id: grantId });
 }
+// function findById(grantId) {
+//   return db("grant_applications").join("grants", "grant_applications.grant_id", "=", "grants.id").select("grants.user_id")
+// }
 
 // creates a new row in the grants_applications table
 function add(application) {
   return db("grant_applications").insert(application);
+}
+
+// below updates the application status from the grantor dashboard
+function updateStatus(applicationId, newStatus) {
+  // the new status must be an integer because we used keys
+  return db("grant_applications")
+    .update({ status: newStatus })
+    .where({ id: applicationId });
+  // .then(function(count) {
+  //   console.log(count);
+  // });
+}
+
+// BELOW ENDPOINT SHOULD NEVER BE HIT MORE THAN 3 TIMES
+// ONLY NEEDED TO CREATE THE STATUS KEYS
+
+function createStatus(status) {
+  return db("application_status").insert(status);
 }
